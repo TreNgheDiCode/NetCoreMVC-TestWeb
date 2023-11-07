d68e5cc6a60f69c032d0a046781477acb563e1f0
TreNgheDiCode <gabayan170@gmail.com>
2023-11-07 22:55:39 +0700

    Info Books

---
+++ Controllers/BookController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestWeb.Data;
+
+namespace TestWeb.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly DataContext context;
+
+        public BookController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Detail(string name)
+        {
+            var book = await context.Books.FirstOrDefaultAsync(book => book.name == name);
+            return View(book);
+        }
+    }
+}
